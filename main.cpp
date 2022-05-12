879290c58cad3bd2cbea2a7db06c1009847ef302
TomD <stahet.infosec@gmail.com>
2022-05-12 09:12:54 +0200

    First commit, does it work ????

--- main.cpp
+++ main.cpp
@@ -1,11 +1,89 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
 
-int main(int argc, char *argv[])
+class ExceptionBounds{
+
+};
+
+class ExceptionCommand{
+
+};
+
+bool deplacer_personnage(int &x, int &y, std::string cmd);
+
+
+int main(int argc, char** argv)
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    int x=5, y=4;
+    std::string saisie;
+    while (true){
+    std::getline(std::cin,saisie);
+    std::cout << "Vous avez saisi : " << saisie << std::endl;
+    try {
+        deplacer_personnage(x,y, saisie);
+    }  catch (ExceptionBounds& e) {
+          std::cout << "Vous sortez des limites" << std::endl;
+          deplacer_personnage(x,y, saisie);
+    }
+    catch (ExceptionCommand& e){
+        std::cout << "Vous n'avez pas rentré une commande valide" << std::endl;
+    }
+
+    std::cout << x << " et " << y << std::endl;
+    }
+    return 0;
+}
+
+bool deplacer_personnage(int &x, int &y, std::string cmd){
+    const int xmin = 0;
+    const int xmax = 32;
+    const int ymin = 0;
+    const int ymax = 15;
+
+    if (cmd == "RIGHT"){
+        if (x+1 > xmax){
+            throw ExceptionBounds();
+        }
+        else{
+            x++;
+            return true;
+        }
+    }
+    else if (cmd == "LEFT"){
+        if (x-1 < xmin){
+            throw ExceptionBounds();
+        }
+        else{
+            x--;
+            return true;
+        }
+    }
+    else if (cmd == "UP"){
+        if (y-1 < ymin){
+            throw ExceptionBounds();
+        }
+        else{
+            y--;
+            return true;
+        }
+    }
+    else if (cmd == "DOWN"){
+        if (y+1 > ymax){
+            throw ExceptionBounds();
+        }
+        else{
+            y++;
+            return true;
+        }
+    }
+    else if (cmd == "IDLE"){
+          return false;
+    }
+    else{
+        throw ExceptionCommand();
+    }
+
 }
