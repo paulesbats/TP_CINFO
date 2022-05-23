4be63c060d56f0f78122c58c7d6bb9d50b0761e8
Polo <polo@git.com>
2022-05-23 09:31:21 +0200

    test

--- notifiercollision.cpp
+++ notifiercollision.cpp
@@ -7,7 +7,6 @@ NotifierCollision::NotifierCollision()
 {
 
 }
-<<<<<<< HEAD
 void NotifierCollision::notify(){
     if (test_collision(getpos_x(),getpos_y()){
             std::cout<<"il y a collision"<<std::endl;
@@ -15,8 +14,8 @@ void NotifierCollision::notify(){
     else {
             std::cout<<"pas de collision"<<std::endl;
     }
-=======
 std::string NotifierCollision::notify(int x,int y){
     test_collision(int x,int y);
->>>>>>> 1a94dd9b347360118ce0fc6b61d026d6c6dd47c5
 }
+
+// coucou
